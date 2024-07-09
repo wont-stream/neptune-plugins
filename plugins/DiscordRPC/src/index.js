929f9f63c6c53b9320e7e2f5643edfb515321a5e
Unstream <drns.pw@icloud.com>
2024-07-09 18:02:43 -0400

    update plugin

--- plugins/DiscordRPC/src/index.js
+++ plugins/DiscordRPC/src/index.js
@@ -1,84 +1,65 @@
 import { store, intercept, currentMediaItem } from "@neptune";
 import { getMediaURLFromID } from "@neptune/utils";
-import { AutoClient } from "./rpc.js";
-import { html } from "@neptune/voby";
-import { storage } from "@plugin";
+import WebSocketTransport from "./ws.js";
 
 const unloadables = [];
-const clientId = "1130698654987067493";
-
-storage.keepRpcOnPause ??= true;
 
 const formatLongString = (s) => (s.length >= 128 ? s.slice(0, 125) + "..." : s);
 
-const rpc = new AutoClient({ transport: "ws" });
-const client = rpc.endlessLogin({ clientId });
+const ws = new WebSocketTransport();
 
-client.then(() => {
-  unloadables.push(
-    intercept("playbackControls/TIME_UPDATE", ([current]) => {
-      const state = store.getState();
+unloadables.push(
+  intercept("playbackControls/TIME_UPDATE", ([current]) => {
+    const state = store.getState();
 
-      const { item: currentlyPlaying, type: mediaType } = currentMediaItem;
+    const { item: currentlyPlaying, type: mediaType } = currentMediaItem;
 
-      // TODO: add video support
-      if (mediaType != "track") return;
+    // TODO: add video support
+    if (mediaType != "track") return;
 
-      const albumArtURL = getMediaURLFromID(currentlyPlaying.album.cover);
+    const albumArtURL = getMediaURLFromID(currentlyPlaying.album.cover);
 
-      const date = new Date();
-      const now = (date.getTime() / 1000) | 0;
-      const remaining = date.setSeconds(
-        date.getSeconds() + (currentlyPlaying.duration - current)
-      );
+    const date = new Date();
+    const now = (date.getTime() / 1000) | 0;
+    const remaining = date.setSeconds(
+      date.getSeconds() + (currentlyPlaying.duration - current)
+    );
 
-      const paused = state.playbackControls.playbackState == "NOT_PLAYING";
+    const paused = state.playbackControls.playbackState == "NOT_PLAYING";
 
-      if (paused && storage.keepRpcOnPause === false) {
-        return rpc.clearActivity();
-      }
-
-      rpc.setActivity({
-        ...(paused
-          ? {
-              smallImageKey: "paused-icon",
-              smallImageText: "Paused",
-            }
-          : {
-              startTimestamp: now,
-              endTimestamp: remaining,
-            }),
-        details: formatLongString(currentlyPlaying.title),
-        state: formatLongString(
-          "by " + currentlyPlaying.artists.map((a) => a.name).join(", ")
-        ),
-        largeImageKey: albumArtURL,
-        largeImageText: formatLongString(currentlyPlaying.album.title),
-      });
-    })
-  );
-});
+    ws.send({
+      cmd: "SET_ACTIVITY",
+      args: {
+        pid: 2094112,
+        activity: {
+          ...(paused
+            ? {
+                smallImageKey: "paused-icon",
+                smallImageText: "Paused",
+              }
+            : {
+                startTimestamp: now,
+                endTimestamp: remaining,
+              }),
+          type: 2,
+          name: formatLongString(currentlyPlaying.title),
+          details: formatLongString(
+            "by " + currentlyPlaying.artists.map((a) => a.name).join(", ")
+          ),
+          largeImageKey: albumArtURL,
+          largeImageText: `on ${formatLongString(
+            currentlyPlaying.album.title
+          )}`,
+        },
+      },
+    });
+  })
+);
 
 export async function onUnload() {
-  const resolvedClient = await client;
   unloadables.forEach((u) => u());
 
   try {
-    resolvedClient.clearActivity();
-    resolvedClient.destroy();
+    ws.close();
   } catch {}
-}
-
-export function Settings() {
-  return html`
-    <div style="display:flex;justify-content:space-between">
-      <label for="keep-rpc-on-pause">Keep RPC on pause</label>
-      <input
-        id="keep-rpc-on-pause"
-        type="checkbox"
-        checked=${storage.keepRpcOnPause}
-        onChange=${(e) => (storage.keepRpcOnPause = e.target.checked)}
-      />
-    </div>
-  `;
 }
