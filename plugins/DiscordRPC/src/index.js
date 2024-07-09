e98aed030ac098b0a740429f83a9829c63f94e63
Unstream <drns.pw@icloud.com>
2024-07-09 18:14:17 -0400

    slight changes

--- plugins/DiscordRPC/src/index.js
+++ plugins/DiscordRPC/src/index.js
@@ -6,55 +6,57 @@ const unloadables = [];
 
 const formatLongString = (s) => (s.length >= 128 ? s.slice(0, 125) + "..." : s);
 
-const ws = new WebSocketTransport();
+const ws = new WebSocketTransport(() => {});
 
-unloadables.push(
-  intercept("playbackControls/TIME_UPDATE", ([current]) => {
-    const state = store.getState();
+ws.connect().then(() => {
+  unloadables.push(
+    intercept("playbackControls/TIME_UPDATE", ([current]) => {
+      const state = store.getState();
 
-    const { item: currentlyPlaying, type: mediaType } = currentMediaItem;
+      const { item: currentlyPlaying, type: mediaType } = currentMediaItem;
 
-    // TODO: add video support
-    if (mediaType != "track") return;
+      // TODO: add video support
+      if (mediaType != "track") return;
 
-    const albumArtURL = getMediaURLFromID(currentlyPlaying.album.cover);
+      const albumArtURL = getMediaURLFromID(currentlyPlaying.album.cover);
 
-    const date = new Date();
-    const now = (date.getTime() / 1000) | 0;
-    const remaining = date.setSeconds(
-      date.getSeconds() + (currentlyPlaying.duration - current)
-    );
+      const date = new Date();
+      const now = (date.getTime() / 1000) | 0;
+      const remaining = date.setSeconds(
+        date.getSeconds() + (currentlyPlaying.duration - current)
+      );
 
-    const paused = state.playbackControls.playbackState == "NOT_PLAYING";
+      const paused = state.playbackControls.playbackState == "NOT_PLAYING";
 
-    ws.send({
-      cmd: "SET_ACTIVITY",
-      args: {
-        pid: 2094112,
-        activity: {
-          ...(paused
-            ? {
-                smallImageKey: "paused-icon",
-                smallImageText: "Paused",
-              }
-            : {
-                startTimestamp: now,
-                endTimestamp: remaining,
-              }),
-          type: 2,
-          name: formatLongString(currentlyPlaying.title),
-          details: formatLongString(
-            "by " + currentlyPlaying.artists.map((a) => a.name).join(", ")
-          ),
-          largeImageKey: albumArtURL,
-          largeImageText: `on ${formatLongString(
-            currentlyPlaying.album.title
-          )}`,
+      ws.send({
+        cmd: "SET_ACTIVITY",
+        args: {
+          pid: 2094112,
+          activity: {
+            ...(paused
+              ? {
+                  smallImageKey: "paused-icon",
+                  smallImageText: "Paused",
+                }
+              : {
+                  startTimestamp: now,
+                  endTimestamp: remaining,
+                }),
+            type: 2,
+            name: formatLongString(currentlyPlaying.title),
+            details: formatLongString(
+              "by " + currentlyPlaying.artists.map((a) => a.name).join(", ")
+            ),
+            largeImageKey: albumArtURL,
+            largeImageText: `on ${formatLongString(
+              currentlyPlaying.album.title
+            )}`,
+          },
         },
-      },
-    });
-  })
-);
+      });
+    })
+  );
+});
 
 export async function onUnload() {
   unloadables.forEach((u) => u());
