6a5e92245f4ec928702ba0158dd6929d0e33ffcf
Unstream <143244075+wont-stream@users.noreply.github.com>
2024-07-11 00:18:51 -0400

    Update index.js

--- plugins/DiscordRPC/src/index.js
+++ plugins/DiscordRPC/src/index.js
@@ -39,7 +39,7 @@ ws.onopen = () => {
                 ...(paused
                   ? {}
                   : {
-                      //start: now,
+                      start: now,
                       end: remaining,
                     }),
               },
