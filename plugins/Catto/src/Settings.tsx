d3d32eaa1b6e8a34d6f804e9a98060b368a032e6
seth <git@ipv4.army>
2025-05-21 14:51:35 -0400

    Remove dev opt, add proper css, fix mobile mode, add comments, run stuff in scopes

--- plugins/Catto/src/Settings.tsx
+++ plugins/Catto/src/Settings.tsx
@@ -5,33 +5,22 @@ import { LunaSettings, LunaNumberSetting, LunaSwitchSetting } from "@luna/ui";
 import { ReactiveStore } from "@luna/core";
 
 export const storage = await ReactiveStore.getPluginStorage("CatJam", {
-    opacity: 80,
-    skipNoBPM: false,
+    opacity: 80
 });
 
 import element from ".";
 
 export const Settings = () => {
     const [opacity, setOpacity] = React.useState(storage.opacity);
-    const [skipNoBPM, setSkipNoBPM] = React.useState(storage.skipNoBPM);
 
     const onChange = React.useCallback((value: number) => {
-        element.style.opacity = `${value / 100}`;
+        element.style = `--catjam-opacity: ${value / 100};`;
         setOpacity((storage.opacity = value));
     }, []);
 
     return (
         <LunaSettings>
             <LunaNumberSetting title="CatJam Opacity" desc="Adjust the opacity of the CatJam buddy" min={0} max={100} step={1} value={opacity} onNumber={onChange} />
-            <LunaSwitchSetting
-                title="Auto Skip"
-                desc="Skip a track if no BPM is given."
-                tooltip="Enable auto skip"
-                checked={skipNoBPM}
-                onChange={(_, checked) => {
-                    setSkipNoBPM((storage.skipNoBPM = checked));
-                }}
-            />
         </LunaSettings>
     );
 };
--- plugins/Catto/src/index.ts
+++ plugins/Catto/src/index.ts
@@ -1,31 +1,39 @@
 import type { LunaUnload } from "@luna/core";
-import { redux, MediaItem, PlayState } from "@luna/lib";
+import { redux, MediaItem, PlayState, StyleTag } from "@luna/lib";
 import { storage } from "./Settings";
 
 export { Settings } from "./Settings";
 
-let element: HTMLVideoElement = document.createElement("video");
-element.src = "https://cdn.jsdelivr.net/gh/wont-stream/neptune-plugins@dev/plugins/Catto/src/catjam.webm";
-element.style.margin = "0 0 0 8px";
-element.style.height = "64px";
-element.style.position = "relative";
-element.style.top = "-63px";
-element.style.left = "-8px";
-element.style.opacity = (storage.opacity / 100).toString();
-element.style.borderRadius = "var(--wave-border-radius--extra-small)";
-element.loop = true;
-element.muted = true;
+export const unloads = new Set<LunaUnload>();
 
-const player = document.querySelector("[data-test=\"current-media-imagery\"]");
+import css from "file://index.css"
 
-if (player) {
-	player.append(element);
+{ // Add CSS
+	new StyleTag("CatJam", unloads, css).add();
 }
 
-export const unloads = new Set<LunaUnload>();
+let element: HTMLVideoElement = document.createElement("video");
+{ // Setup catjam element
+	element.src = "https://cdn.jsdelivr.net/gh/wont-stream/neptune-plugins@dev/plugins/Catto/src/catjam.webm";
+
+	element.classList.add("CatJam");
+	element.style = `--catjam-opacity: ${storage.opacity / 100};`;
+
+	element.loop = true;
+	element.muted = true;
+}
+
+{ // Add catjam element to the player
+	const player = document.querySelector("[data-test=\"current-media-imagery\"]");
+
+	if (player) {
+		player.append(element);
+	}
+}
+
 unloads.add(() => element?.remove());
 
-redux.intercept(["playbackControls/MEDIA_PRODUCT_TRANSITION", "playbackControls/SET_PLAYBACK_STATE"], unloads, async ({ mediaProduct }) => {
+redux.intercept(["playbackControls/MEDIA_PRODUCT_TRANSITION", "playbackControls/SET_PLAYBACK_STATE"], unloads, async () => {
 	const mediaItem = await MediaItem.fromPlaybackContext();
 
 	if (!mediaItem) {
@@ -36,10 +44,6 @@ redux.intercept(["playbackControls/MEDIA_PRODUCT_TRANSITION", "playbackControls/
 
 	const bpm = await mediaItem?.bpm()
 
-	if (!bpm) {
-		if (storage.skipNoBPM) PlayState.next();
-	}
-
 	if (PlayState.playing) {
 		element.play();
 
@@ -47,7 +51,6 @@ redux.intercept(["playbackControls/MEDIA_PRODUCT_TRANSITION", "playbackControls/
 			//element.currentTime = 0;
 			element.playbackRate = bpm / 135.48;
 		} else {
-			if (storage.skipNoBPM) PlayState.next();
 			element.pause();
 			element.currentTime = 0;
 		}
