d3c139d8d4d30084068f21d0ba58e97a049afed8
seth <git@ipv4.army>
2025-05-22 04:07:41 -0400

    stablejam maybe

--- plugins/CatJam/src/index.ts
+++ plugins/CatJam/src/index.ts
@@ -8,56 +8,109 @@ export const unloads = new Set<LunaUnload>();
 
 import css from "file://index.css"
 
-{ // Add CSS
-	new StyleTag("CatJam", unloads, css).add();
-}
+//#region CSS
+new StyleTag("CatJam", unloads, css).add();
+//#endregion
 
+
+
+//#region CatJam Video Element
 let video: HTMLVideoElement = document.createElement("video");
-{ // Setup catjam element
-	video.src = "https://cdn.jsdelivr.net/gh/wont-stream/neptune-plugins@dev/plugins/Catto/src/catjam.webm";
 
-	video.classList.add("CatJam");
-	video.style = `--catjam-opacity: ${storage.opacity / 100};`;
+video.src = "https://cdn.jsdelivr.net/gh/wont-stream/neptune-plugins@dev/plugins/Catto/src/catjam.webm";
 
-	video.loop = true;
-	video.muted = true;
+video.classList.add("CatJam");
+video.style = `--catjam-opacity: ${storage.opacity / 100};`;
+
+video.loop = true;
+video.muted = true;
+//#endregion
+
+//#region Append CatJam to the media imagery
+const appendCatJam = () => {
+	const smallAlbumArt = document.querySelector("[data-test=\"current-media-imagery\"]");
+	if (smallAlbumArt) return smallAlbumArt.append(video);
+
+	return setTimeout(appendCatJam, 100)
 }
+appendCatJam();
+//#endregion
 
-{ // Add catjam element to the player
-	const player = document.querySelector("[data-test=\"current-media-imagery\"]");
-
-	if (player) {
-		player.append(video);
-	}
-}
-
+//#region Add CatJam to unloads
 unloads.add(() => video?.remove());
+//#endregion
 
-redux.intercept(["playbackControls/MEDIA_PRODUCT_TRANSITION", "playbackControls/SET_PLAYBACK_STATE"], unloads, async () => {
-	const mediaItem = await MediaItem.fromPlaybackContext();
+//#region Variables
+let hasBPM = false;
+let duration = 0;
+//#endregion
 
-	if (!mediaItem) {
-		return;
-	}
+//#region Set the current time of the video
+const setCurrentTime = () => {
+	return video.currentTime = ((duration || 0) - PlayState.playTime) % video.duration;
+}
+//#endregion
 
-	video.currentTime = ((mediaItem.duration || 0) - PlayState.playTime) % video.duration;
+//#region Update CatJam
+const updateCatJam = async ({ type, mediaItem, playbackState }: { type: "mediaTransition" | "playbackState" | "load", mediaItem?: MediaItem | undefined, playbackState?: redux.PlaybackState | undefined }) => {
+	switch (type) {
+		case "mediaTransition": {
+			if (mediaItem) {
+				const bpm = await mediaItem?.bpm();
 
-	const bpm = await mediaItem?.bpm()
+				duration = mediaItem?.duration || 0;
+				setCurrentTime();
 
-	if (PlayState.playing) {
-		video.play();
-
-		if (bpm) {
-			//video.currentTime = 0;
-			video.playbackRate = bpm / 135.48;
-		} else {
-			video.pause();
-			video.currentTime = 0;
+				if (bpm) {
+					hasBPM = true;
+					video.playbackRate = bpm / 135.48;
+				} else {
+					hasBPM = false;
+					video.pause();
+					video.currentTime = 0;
+				}
+			}
+			break;
 		}
-	} else {
-		video.pause();
-		//video.currentTime = 0;
+		case "playbackState": {
+			switch (playbackState) {
+				case "PLAYING": {
+					if (hasBPM) {
+						setCurrentTime();
+						video.play();
+					} else {
+						video.pause();
+					}
+					break;
+				}
+				default: {
+					video.pause();
+					break;
+				}
+			}
+			break;
+		}
+		case "load": {
+			await updateCatJam({ type: "mediaTransition", mediaItem: await MediaItem.fromPlaybackContext() });
+			await updateCatJam({ type: "playbackState", playbackState: PlayState.playing ? "PLAYING" : "PAUSED" });
+			break;
+		}
 	}
-});
+}
+//#endregion
+
+//#region Listeners
+MediaItem.onMediaTransition(unloads, async (mediaItem) => {
+	return await updateCatJam({ type: "mediaTransition", mediaItem, playbackState: "PAUSED" });
+})
+
+PlayState.onState(unloads, async (playbackState) => {
+	return await updateCatJam({ type: "playbackState", playbackState });
+})
+
+video.onloadeddata = async () => {
+	return await updateCatJam({ type: "load" });
+}
+//#endregion
 
 export default video;
